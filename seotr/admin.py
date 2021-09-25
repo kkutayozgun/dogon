599b68cd40bbc80a1e2aefb1af587a02c1ca6fc3
kkutayozgun <kkutayozgun@gmail.com>
2021-09-25 14:34:05 +0300

    admin page field sirasi degisti

--- seotr/admin.py
+++ seotr/admin.py
@@ -17,51 +17,78 @@ from seotr.models import (
 )
 
 
+SEO_FIELDS = ('seo_title', 'meta_description', 'meta_keywords')
+
 admin.site.register(KeywordsTr)
-admin.site.register(HomeSeoTr)
-admin.site.register(Slider)
-admin.site.register(AboutTr)
+
+
+@admin.register(HomeSeoTr)
+class HomeSeoTrAdmin(admin.ModelAdmin):
+    fields = SEO_FIELDS
+
+
+@admin.register(Slider)
+class SliderAdmin(admin.ModelAdmin):
+    fields = ('title_layer1', 'title_layer2',
+              'body', 'button_url', 'sliderimage')
+
+
+@admin.register(AboutTr)
+class AboutTrAdmin(admin.ModelAdmin):
+    fields = ('banner', 'banner_image', 'title1', 'body1',
+              'image1', 'title2', 'body2', 'image2') + SEO_FIELDS
+
+
 admin.site.register(ReferenceTr)
-admin.site.register(ApplicationTr)
 
 
+@admin.register(ApplicationTr)
+class ApplicationTrAdmin(admin.ModelAdmin):
+    fields = ('banner_title', 'banner_image') + SEO_FIELDS
+
+
+@admin.register(ApplicationCategoryTr)
 class ApplicationCategoryTrAdmin(admin.ModelAdmin):
-    fields = ('title', 'slug', 'banner_title', 'banner_image',
-              'seo_title', 'meta_description', 'meta_keywords')
+    fields = ('title', 'slug', 'banner_title', 'banner_image') + SEO_FIELDS
     prepopulated_fields = {'slug': ('title',), }
 
 
-admin.site.register(ApplicationCategoryTr, ApplicationCategoryTrAdmin)
-
+@admin.register(ApplicationSubCategoryTr)
 class ApplicationSubCategoryTrAdmin(admin.ModelAdmin):
-    fields = ('title', 'slug', 'banner_title', 'body', 'cover_image', 'category', 'image', 'pdf_file',
-              'seo_title', 'meta_description', 'meta_keywords')
+    fields = ('title', 'slug', 'banner_title', 'body', 'cover_image',
+              'category', 'image', 'pdf_file') + SEO_FIELDS
     prepopulated_fields = {'slug': ('title',), }
 
 
-admin.site.register(ApplicationSubCategoryTr, ApplicationSubCategoryTrAdmin)
+@admin.register(QualityTr)
+class QualityTrAdmin(admin.ModelAdmin):
+    fields = ('banner_title', 'banner_image',
+              'title', 'body', 'image') + SEO_FIELDS
 
 
-admin.site.register(QualityTr)
+@admin.register(BlogPageSeoTr)
+class BlogPageSeoTrAdmin(admin.ModelAdmin):
+    fields = ('banner_title', 'banner_image') + SEO_FIELDS
 
-admin.site.register(BlogPageSeoTr)
 
+@admin.register(BlogCategoryTr)
 class BlogCategoryTrAdmin(admin.ModelAdmin):
-    fields = ('title', 'slug', 'banner_title',
-              'seo_title', 'meta_description', 'meta_keywords')
+    fields = ('title', 'slug', 'banner_title') + SEO_FIELDS
     prepopulated_fields = {'slug': ('title',), }
 
 
-admin.site.register(BlogCategoryTr, BlogCategoryTrAdmin)
-
+@admin.register(BlogContentTr)
 class BlogContentTrAdmin(admin.ModelAdmin):
-    fields = ('category','title', 'slug', 'banner_title', 'body', 'image',
-              'seo_title', 'meta_description', 'meta_keywords')
+    fields = ('category', 'banner_title', 'title',
+              'slug', 'body', 'image') + SEO_FIELDS
     prepopulated_fields = {'slug': ('title',), }
 
 
-admin.site.register(BlogContentTr, BlogContentTrAdmin)
+@admin.register(ContactSeoTr)
+class ContactSeoTrAdmin(admin.ModelAdmin):
+    fields = ('banner_title', 'banner_image') + SEO_FIELDS
 
-admin.site.register(ContactSeoTr)
 
-admin.site.register(KVKKAydınlatmaMetniTr)
+@admin.register(KVKKAydınlatmaMetniTr)
+class KVKKAydınlatmaMetniTrAdmin(admin.ModelAdmin):
+    fields = ('banner_title', 'banner_image', 'body') + SEO_FIELDS
